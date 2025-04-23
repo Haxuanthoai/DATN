ada67b0a9f733277dba4e0bb81f300b05697b7fb
Haxuanthoai <154058813+Haxuanthoai@users.noreply.github.com>
2025-04-23 15:21:52 +0700

    update

---
+++ OfficeFurnitureStore/Areas/AdminStore/Controllers/DashboardController.cs
@@ -0,0 +1,24 @@
+﻿using OfficeFurnitureStore.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace OfficeFurnitureStore.Areas.AdminStore.Controllers {
+
+    public class DashboardController : BaseController
+    {
+        private readonly OfficeFurnitureStoreContext _context;
+
+        public DashboardController(OfficeFurnitureStoreContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+
+            return View();
+        }
+    }
+
+}
